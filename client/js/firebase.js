459e4ff955e21be15bc3ce9c29d9f8be23e46c9e
Angelina1379 <lina.pozolotina.03@mail.ru>
2026-05-28 16:37:29 +0400

    Update firebase.js

--- client/js/firebase.js
+++ client/js/firebase.js
@@ -1,27 +1,37 @@
-// Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Config
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyBuV2dJLDPxsk6E50e5p0E5Buk_NAc46-E",
+
   authDomain: "remont-f7644.firebaseapp.com",
+
   projectId: "remont-f7644",
-  storageBucket: "remont-f7644.appspot.com",
+
+  storageBucket: "remont-f7644.firebasestorage.app",
+
   messagingSenderId: "404840020044",
+
   appId: "1:404840020044:web:f81c9613321ac170c19ce9",
+
   measurementId: "G-J7S537NP48"
+
 };
 
-// Init
 const app = initializeApp(firebaseConfig);
 
-// Services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-// Export ONLY THIS
-export { auth, db, storage };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
